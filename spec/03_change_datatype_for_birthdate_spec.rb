05ddabb426567b8afa218207ecd751754cdb9d4b
thakaj <taulant.hakaj@scranton.edu>
2021-08-08 12:16:13 -0400

    commit tree 88d8d118735d0e075947ca578134c309f25662e6

--- spec/03_change_datatype_for_birthdate_spec.rb
+++ spec/03_change_datatype_for_birthdate_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../db/migrate/01_create_students'
-require_relative '../db/migrate/02_add_grade_and_birthdate_to_students'
+require_relative '../db/migrate/02_add_grade_and_birthdate_to_students_spec.rb'
 require_relative '../db/migrate/03_change_datatype_for_birthdate'
 
 describe ChangeDatatypeForBirthdate do
